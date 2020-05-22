546026a72ef262c878323978d29d3d7eb1d98369
Ian Laird <irlaird@gmail.com>
2020-05-21 23:54:50 -0600

    Added some arch stuff

--- modules/nvim/module.sh
+++ modules/nvim/module.sh
@@ -31,8 +31,32 @@ ubuntu_remove() {
     rm -rf "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim
 }
 
+arch_install() {
+    sudo pacman -Syu neovim python python2 python2-virtualenv
+    sudo install -o root -g root -m 755 -d $NVIM_SHARE_TARGET/share/nvim
+
+    # install python2 virtual env
+    sudo python2 -m virtualenv $NVIM_SHARE_TARGET/share/nvim/python2
+    sudo $NVIM_SHARE_TARGET/share/nvim/python2/bin/pip install pynvim
+
+    # install python3 virtual env
+    sudo python3 -m venv $NVIM_SHARE_TARGET/share/nvim/python3
+    sudo $NVIM_SHARE_TARGET/share/nvim/python3/bin/pip install pynvim
+
+    # install vim-plug
+    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+}
+
+arch_remove() {
+    sudo unlink $NVIM_SHARE_TARGET/bin/nvim
+    sudo rm -rf $NVIM_SHARE_TARGET/share/nvim
+    rm -rf "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim
+}
+
 _install() {
     has_platform ubuntu18 && ubuntu_install
+    has_platform arch && arch_install
     mkdir -p ~/.config/nvim/init.d
     ln -s $CMD/init.vim ~/.config/nvim/init.vim
     for f in $(find $CMD/init.d -type f)
@@ -44,6 +68,7 @@ _install() {
 
 _remove() {
     has_platform ubuntu18 && ubuntu_remove
+    has_platform arch && arch_remove
     rm -rf ~/.config/nvim
 }
 
---
+++ modules/powerline-fonts/module.sh
@@ -0,0 +1,22 @@
+source $DOTFILES_TOOL_PATH/common.sh
+
+MD=$DOTFILES_CURRENT_MOD_DIR
+ENV=$DOTFILES_ENV
+
+_install() {
+    git clone https://github.com/powerline/fonts
+    cd fonts
+    sudo ./install.sh
+}
+
+_remove() {
+    echo "Sorry, you are on your own."
+}
+
+_info() {
+    echo -n "Powerline Fonts - patched fonts with extra glyphs" && \
+        is_installed "$ENV.d/workspace.env" && \
+        echo " - [Installed]" || echo ""
+}
+
+run_if ubuntu18 arch osx
