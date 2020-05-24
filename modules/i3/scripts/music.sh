856ecbb28ee15032e467b21507ab622eb002b94e
Ian Laird <irlaird@gmail.com>
2020-05-24 12:18:55 -0600

    fixed dmenu colors.

--- modules/i3/scripts/music.sh
+++ modules/i3/scripts/music.sh
@@ -4,10 +4,11 @@ WHITE='#918175'
 BLACK='#1C1B19'
 XGREY3='#3A3A3A'
 ORANGE='#D75F00'
+YELLOW='#FBB829'
 XFONT='-*-dejavu sans mono-medium-r-normal--13-80-*-*-*-*-iso10646-1'
 
 CMD=playerctl
-DMENU="dmenu -nf $WHITE -nb $BLACK -sb $XGREY3 -sf $ORANGE -p Spotify"
+DMENU="dmenu -nf $WHITE -nb $BLACK -sb $XGREY3 -sf $YELLOW -p Spotify"
 
 function run-spotify() {
     ps aux | grep -v grep  | grep spotify > /dev/null
