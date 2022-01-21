c9927637a7fb5622b36cee498c01cfa01893cf17
Ian Laird <irlaird@gmail.com>
2022-01-20 20:37:36 -0700

    Added layouts for when spock is docked.

--- modules/i3/scripts/launch-polybar.sh
+++ modules/i3/scripts/launch-polybar.sh
@@ -4,6 +4,7 @@ for opt in $@
 do
     [[ "primary" == "$opt" ]] && PRIMARY="yes"
     [[ "secondary" == "$opt" ]] && SECONDARY="yes"
+    [[ "tertiary" == "$opt" ]] && TERTIARY="yes"
 done
 
 killall -q polybar
@@ -11,3 +12,4 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 [ ! -z "$PRIMARY" ] && polybar primary -r &
 [ ! -z "$SECONDARY" ] && polybar secondary -r &
+[ ! -z "$TERTIARY" ] && polybar tertiary -r &
