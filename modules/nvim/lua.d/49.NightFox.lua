f8dd455c47afccd18b63639d15ac7a9f2768d152
Ian Laird <ian.laird@cradlepoint.com>
2022-11-01 10:53:43 -0600

    added theme updates to alacritty

---
+++ modules/nvim/lua.d/49.NightFox.lua
@@ -0,0 +1,9 @@
+require('nightfox').setup({
+  options = {
+    inverse = {             -- Inverse highlight for different types
+      match_paren = true,
+      visual = true,
+      search = true
+    }
+  }
+})
